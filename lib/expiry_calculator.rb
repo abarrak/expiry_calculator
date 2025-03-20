571834bf78fe55332765315454fd6568e628b8f3
Abdullah Barrak <a@abarrak.com>
2025-03-20 15:02:35 +0300

    fix inclusion & improve module docs
    Signed-off-by: Abdullah Barrak <a@abarrak.com>

--- lib/expiry_calculator.rb
+++ lib/expiry_calculator.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require_relative "expiry_calculator/logic"
 require_relative "expiry_calculator/version"
 
 module ExpiryCalculator
+  include ExpiryCalculator::Logic
 end
--- lib/expiry_calculator/logic.rb
+++ lib/expiry_calculator/logic.rb
@@ -1,18 +1,30 @@
 module ExpiryCalculator
-  ##
-  # A function to calculate the expiration from a given date.
-  #
-  def calculate record, record_attr = nil
-    expiry_date = case record
-      when Date
-        record
-      when DateTime
-        record
-      when ApplicationRecord
-        record[record_attr]
-      else
+  module Logic
+    ##
+    # A function to calculate the expiration from a given date.
+    #
+    # == Parameters:
+    #
+    # [*record*] +Date|DateTime|ApplicationRecord+: field of date and time to caluclate expiry for. Can be activerecord model containing a date field.
+    # [*record_attr*] +symbol|string+ The date attribute field in case of active record model is passed in first arguemtn. Default to nil.
+    #
+    # == Returns:
+    #
+    # The remaining days till the expiry, or zero if it already past.
+    #
+    #
+    def calculate record, record_attr = nil
+      expiry_date = case record
+        when Date
+          record
+        when DateTime
+          record
+        when ApplicationRecord
+          record[record_attr.to_sym]
+        else
+      end
+      days = (Date.today..expiry_date).count
+      days.zero? ? 0 : days - 1
     end
-    days = (Date.today..expiry_date).count
-    days.zero? ? 0 : days - 1
   end
 end
