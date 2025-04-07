9907870649850edff4d33355249397d2814c8b88
Abdullah Barrak <a@abarrak.com>
2025-04-07 09:07:23 +0300

    revise attr docs and polish
    Signed-off-by: Abdullah Barrak <a@abarrak.com>

--- lib/expiry_calculator/logic.rb
+++ lib/expiry_calculator/logic.rb
@@ -5,13 +5,16 @@ module ExpiryCalculator
     #
     # == Parameters:
     #
-    # [*record*] +Date|DateTime|ApplicationRecord+: field of date and time to caluclate expiry for. Can be activerecord model containing a date field.
+    # [*record*] +Date|DateTime|String|ApplicationRecord+: field of date and time to caluclate expiry for. Can be activerecord model containing a date field.
     # [*record_attr*] +symbol|string+ The date attribute field in case of active record model is passed in first arguemtn. Default to nil.
     #
     # == Returns:
     #
     # The remaining days till the expiry, or zero if it already past.
     #
+    # == Note:
+    # ref# for AR special check case:
+    # https://github.com/rails/rails/blob/04cda1848cb847c2bdad0bfc12160dc8d5547775/activerecord/lib/active_record/core.rb#L135
     #
     def calculate record, record_attr = nil
       expiry_date = case record
@@ -21,11 +24,9 @@ module ExpiryCalculator
         record
       when String
         DateTime.parse(record)
-      # https://github.com/rails/rails/blob/04cda1848cb847c2bdad0bfc12160dc8d5547775/activerecord/lib/active_record/core.rb#L135
       when ->(r) { defined?(ActiveRecord::Base) && r.is_a?(ActiveRecord::Base) }
         record[record_attr.to_sym]
       else
-        p record.class
         raise ArgumentError, "record type not supported."
       end
       days = (Date.today..expiry_date).count
